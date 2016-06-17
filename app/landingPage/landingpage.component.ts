66b4aa9a4f2b301b35baf7a26e7c4987c24cd464
Mary Kuchumova <kuchumova.mary@gmail.com>
2016-06-17 15:53:06 +0300

    Bug fix: Landing page photos disappear on reloading page #T4U-61 In Review

--- app/landingPage/landingpage.component.ts
+++ app/landingPage/landingpage.component.ts
@@ -5,18 +5,27 @@
  */
 /// <reference path="../../typings/globals/jquery/index.d.ts" />
 /// <reference path="../../typings/globals/materialize-css/index.d.ts" />
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     moduleId: module.id,
     templateUrl: 'landingpage.component.html'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit{
 
     constructor() {
+        this.initParallax();
+    }
+    
+    ngOnInit() {
+        this.initParallax();
+    }
+
+    initParallax(){
         $(document).ready(function(){
             $('.parallax').parallax();
         });
     }
+
     
 }
