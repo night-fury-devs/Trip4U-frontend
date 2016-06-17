4abfd8ea623f5d093ccc994e0d4d8a9fecafd01b
Alexey Kleschikov <alex.vesy96@gmail.com>
2016-06-17 21:00:50 +0300

    Complete up-button. #T4U-53 In Review

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,11 @@
  * Time: 21:46
  */
 
-import {Component, Type} from '@angular/core';
-import { RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
-import { LandingPageComponent } from "./landingPage/landingpage.component";
-import { EmailConfirmPageComponent } from './email-confirm-page/email-confirm-page.component';
-import { UpButtonComponent } from "./up-button/up-button.component";
+import {Component, Type, OnInit} from '@angular/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+import {LandingPageComponent} from "./landingPage/landingpage.component";
+import {EmailConfirmPageComponent} from './email-confirm-page/email-confirm-page.component';
+import {UpButtonComponent} from "./up-button/up-button.component";
 
 @Component({
     selector: 'my-app',
@@ -20,5 +20,25 @@ import { UpButtonComponent } from "./up-button/up-button.component";
     {path: '/', name: 'App', component: <any>LandingPageComponent, useAsDefault: true},
     {path: '/confirm', name: 'EmailConfirmPage', component: EmailConfirmPageComponent, useAsDefault: false}
 ])
-export class AppComponent {
+export class AppComponent implements OnInit {
+    ngOnInit() {
+        $(document).ready(() => {
+            var up_button = $('#up-button');
+            //Check to see if the window is top if not then display button
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 100) {
+                    up_button.fadeIn(() => up_button.css("opacity", 1));
+                } else {
+                    up_button.fadeOut(() => up_button.css("opacity", 0));
+                }
+            });
+
+            //Click event to scroll to top
+            up_button.click(function () {
+                $('html, body').animate({scrollTop: 0}, 800);
+                return false;
+            });
+
+        });
+    }
 }
