399dea7c3e88e0bcf0d2684d1a1424b210d02b60
Mary Kuchumova <kuchumova.mary@gmail.com>
2016-06-26 16:54:09 +0300

    Add model class for logged in user

---
+++ src/app/pages/login-form/model/LoggedInUser.ts
@@ -0,0 +1,15 @@
+/**
+ * Author: Mary Kuchumova
+ * Date: 26 Jun 2016
+ * Time: 16:52
+ */
+
+
+export class LoggedInUser {
+
+  constructor(
+    userName: string,
+    password: string,
+    rememberMe: boolean
+  ){}
+}
