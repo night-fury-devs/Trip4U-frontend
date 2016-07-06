248d02597b10f870a160d3ab7007c72a3da41a2b
Alexey Kleschikov <alex.vesy96@gmail.com>
2016-07-06 11:51:58 +0300

    Add base layout for user profile page. #T4U-83

---
+++ src/app/pages/user-profile-page/user-profile-page.component.spec.ts
@@ -0,0 +1,9 @@
+/**
+ * Author: Alexey Kleschikov
+ * Date: 06 July 2016
+ * Time: 10:48
+ */
+
+describe('User Profile Page Tests', () => {
+  
+});
---
+++ src/app/pages/user-profile-page/user-profile-page.component.ts
@@ -0,0 +1,19 @@
+/**
+ * Author: Alexey Kleschikov
+ * Date: 06 July 2016
+ * Time: 10:46
+ */
+
+import { Component, Type } from "@angular/core";
+import { ContentPanelComponent } from "../../shared/content-panel/content-panel.component";
+
+@Component({
+  moduleId: module.id,
+  selector: 't4u-user-profile-page',
+  styleUrls: ['user-profile-page.component.css'],
+  templateUrl: 'user-profile-page.component.html',
+  directives: [<Type>ContentPanelComponent]
+})
+export class UserProfilePageComponent {
+  imgSource: string = 'https://pixabay.com/static/uploads/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+}
