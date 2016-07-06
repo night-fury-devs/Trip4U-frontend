eb2af371701ffca62575ddad730e13e050402b90
Alexey Kleschikov <alex.vesy96@gmail.com>
2016-07-06 23:03:40 +0300

    Add some new layout. Need to set up two-way binding.

--- src/app/pages/user-profile-page/user-profile-page.component.ts
+++ src/app/pages/user-profile-page/user-profile-page.component.ts
@@ -6,13 +6,14 @@
 
 import { Component, Type } from "@angular/core";
 import { ContentPanelComponent } from "../../shared/content-panel/content-panel.component";
+import { EditableTextComponent } from "../../shared/editable-text/editable-text.component";
 
 @Component({
   moduleId: module.id,
   selector: 't4u-user-profile-page',
   styleUrls: ['user-profile-page.component.css'],
   templateUrl: 'user-profile-page.component.html',
-  directives: [<Type>ContentPanelComponent]
+  directives: [<Type>ContentPanelComponent, <Type>EditableTextComponent]
 })
 export class UserProfilePageComponent {
   imgSource: string = 'https://pixabay.com/static/uploads/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
--- src/app/shared/editable-text/editable-text.component.ts
+++ src/app/shared/editable-text/editable-text.component.ts
@@ -4,7 +4,7 @@
  * Time: 15:50
  */
 
-import { Component } from "@angular/core";
+import { Component, Input, AfterViewInit } from "@angular/core";
 
 @Component({
   moduleId: module.id,
@@ -12,6 +12,40 @@ import { Component } from "@angular/core";
   templateUrl: 'editable-text.component.html',
   styleUrls: ['editable-text.component.css']
 })
-export class EditableTextComponent {
+export class EditableTextComponent implements AfterViewInit{
+  @Input() text: string;
+
+  ngAfterViewInit() {
+    this.initializeLayout();
+  }
+
+  private initializeLayout() {
+    let input = $('.editable__input');
+    let p = $('.editable__p');
+    
+    input.hide()
+         .focusout(this.finishEdit)
+         .keyup(e => {
+           if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+             this.finishEdit(e);
+             return false;
+           } else {
+             return true;
+           }
+         });
+    p.click(() => {
+      p.hide();
+      input.show().focus();
+    });
+  }
   
+  private finishEdit(event) {
+    let input = $(event.target);
+    let p = input && input.prev();
+
+    p.text(input.val());
+    input.hide();
+    p.show();
+    
+  }
 }
