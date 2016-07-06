88ce9a0eab6becd316ded777881b388cc3deaa20
Alexey Kleschikov <alex.vesy96@gmail.com>
2016-07-06 16:54:43 +0300

    Add component editable-text.

---
+++ src/app/shared/editable-text/editable-text.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Author: Alexey Kleschikov
+ * Date: 06 July 2016
+ * Time: 15:50
+ */
+
+import { Component } from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  selector: 't4u-editable-text',
+  templateUrl: 'editable-text.component.html',
+  styleUrls: ['editable-text.component.css']
+})
+export class EditableTextComponent {
+  
+}
