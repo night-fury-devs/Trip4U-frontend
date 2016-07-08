3024f61fab4309ff6e014b548c6d58bd8fc931fe
Alexey Kleschikov <alex.vesy96@gmail.com>
2016-07-08 17:21:49 +0300

    Add empty home-cities component. #T4U-85

---
+++ src/app/shared/home-cities/home-cities.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Author: Alexey Kleschikov
+ * Date: 08 July 2016
+ * Time: 16:57
+ */
+
+import { Component } from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  selector: 't4u-home-cities',
+  templateUrl: 'home-cities.component.html',
+  styleUrls: ['home-cities.component.css']
+})
+export class HomeCitiesComponent {
+  
+}
