8423b833fe0d804afeb1f28d7c4cd78c72ffe956
Mary Kuchumova <kuchumova.mary@gmail.com>
2016-07-20 00:01:27 +0300

    Implement password checks #T4U-72

--- src/app/pages/registration-form/password/password-strength.directive.ts
+++ src/app/pages/registration-form/password/password-strength.directive.ts
@@ -16,6 +16,12 @@ export class PasswordStrengthDirective {
   private el: HTMLElement;
   private passwordCheckerService: PasswordCheckerService;
 
+  private message: string;
+  private style: string;
+  
+  private WEAK_PASSWORD_RATE = 30;
+  private NORMAL_PASSWORD_RATE = 50;
+  
   constructor(el: ElementRef, passwordCheckerService: PasswordCheckerService) {
     this.el = el.nativeElement;
     this.passwordCheckerService = passwordCheckerService;
@@ -23,26 +29,24 @@ export class PasswordStrengthDirective {
 
   @Input('passwordStrength') password;
   @Output() passwordStrengthChange = new EventEmitter();
-
+  
   @HostListener('keyup') onPasswordChanged() {
     var strengthRate = this.passwordCheckerService.measurePasswordStrength(this.password);
     if(!this.el.classList.contains('error')) {
-      if (strengthRate < 4) {
-        this.passwordStrengthChange.emit({
-          message: 'Weak password',
-          style: 'weak-password'
-        });
-      } else if (strengthRate < 10) {
-        this.passwordStrengthChange.emit({
-          message: 'Normal password',
-          style: 'normal-password'
-        });
+      if (strengthRate <= this.WEAK_PASSWORD_RATE) {
+        this.message = 'Weak password';
+        this.style = 'weak-password';
+      } else if (strengthRate <= this.NORMAL_PASSWORD_RATE) {
+        this.message = 'Normal password';
+        this.style = 'normal-password';
       } else {
-        this.passwordStrengthChange.emit({
-          message: 'Strong password',
-          style: 'strong-password'
-        });
+        this.message = 'Strong password';
+        this.style = 'strong-password';
       }
+      this.passwordStrengthChange.emit({
+        message: this.message,
+        style: this.style
+      })
     } else {
       this.el.classList.remove('strong-password', 'normal-password', 'weak-password');
     }
